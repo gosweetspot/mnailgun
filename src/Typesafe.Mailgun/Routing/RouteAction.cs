8687fd9e8669f6ebe112275f89f345d440364503
Shoaib Shakeel <shoaibshakeel381@yahoo.com>
2016-05-31 16:20:12 +0500

    Added support for store() Api call in RouteActions

--- src/Typesafe.Mailgun/Routing/RouteAction.cs
+++ src/Typesafe.Mailgun/Routing/RouteAction.cs
@@ -19,6 +19,25 @@ namespace Typesafe.Mailgun.Routing
 		{
 			return new RouteAction(string.Format("forward(\"{0}\")", url));
 		}
+        
+	        /// <summary>
+	        /// just store emails temporarily
+	        /// </summary>
+	        /// <returns></returns>
+	        public static RouteAction Store()
+	        {
+	            return new RouteAction(string.Format("store()"));
+	        }
+	
+	        /// <summary>
+	        /// store email and call provided url to notify for new ones
+	        /// </summary>
+	        /// <param name="callbackUri"></param>
+	        /// <returns></returns>
+	        public static RouteAction Store(Uri callbackUri)
+	        {
+	            return new RouteAction(string.Format("store(notify=\"{0}\")", callbackUri));
+	        }
 
 		public static RouteAction Stop()
 		{
